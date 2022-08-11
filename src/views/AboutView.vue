84d8161fe3d02c12f8b6fc84ca1bd144b0bf1582
joycejewell <joyceroop@gmail.com>
2022-08-10 18:23:04 -0600

    added github controller

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,23 @@
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      profile: {},
+    };
+  },
+  created: function () {
+    var githubAccessToken = localStorage.getItem("github_access_token");
+    if (githubAccessToken) {
+      axios.get("/github_profile?github_access_token=" + githubAccessToken).then((response) => {
+        console.log(response.data);
+      });
+    }
+  },
+};
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,10 +5,19 @@ export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
+      posts: [],
     };
   },
   created: function () {
     this.getPostsData();
+    this.getNewsData;
+    var code = this.$route.query.code;
+    if (code) {
+      axios.get("/auth/github/callback?code=" + code).then((response) => {
+        localStorage.setItem("github_access_token", response.data.access_token);
+        this.$router.push("/about");
+      });
+    }
   },
   methods: {
     getPostsData: function () {
@@ -29,6 +38,7 @@ export default {
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <a href="https://github.com/login/oauth/authorize?client_id=1d5f309a11dc7399e62a">Sign into GitHub</a>
     <div v-for="post in posts" v-bind:key="post.id">
       <h2>{{ post.title }}</h2>
       <p>{{ post.body }}</p>
